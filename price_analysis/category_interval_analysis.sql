5f63eabfd0b5a1db071e5f411bad35850de5027e
Teodor-George Calina <teodor-george.calina@lesara.com>
2019-01-29 15:56:10 +0800

    added extra descriptions

--- price_analysis/category_interval_analysis.sql
+++ price_analysis/category_interval_analysis.sql
@@ -19,6 +19,7 @@ SELECT
   ROUND(MAX(price) OVER (PARTITION BY category), 2) AS max_price,
   ROUND(FIRST_VALUE(price) OVER (PARTITION BY category ORDER BY distance_05), 2) AS median_price,
   ROUND(100.0 * STDDEV(price) OVER (PARTITION BY category) / (AVG(price) OVER (PARTITION BY category)), 2) AS stddev_perc,
+  -- Taking the first value ordered by the distance selects that price which is closest to the desired percentile
   ROUND(FIRST_VALUE(price) OVER (PARTITION BY category ORDER BY distance_01), 2) AS price_10_percentile,
   ROUND(FIRST_VALUE(price) OVER (PARTITION BY category ORDER BY distance_09), 2) AS price_90_percentile,
   count(DISTINCT product_id) OVER (PARTITION BY category) AS nr_products
@@ -27,6 +28,8 @@ FROM (
         product_id,
         price,
         category,
+        -- PERCENT_RANK() returns the percntile of the price inside the category
+        -- Calculating the distance between the percentile of the price and the desired percentile helps find the closes price to the desired value
         POW(0.5 - PERCENT_RANK() OVER (PARTITION BY category ORDER BY price), 2) AS distance_05,
         POW(0.1 - PERCENT_RANK() OVER (PARTITION BY category ORDER BY price), 2) AS distance_01,
         POW(0.9 - PERCENT_RANK() OVER (PARTITION BY category ORDER BY price), 2) AS distance_09
